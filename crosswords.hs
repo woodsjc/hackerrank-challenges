c9fe340c5ba5e9a0d04c13d9e8f2bc0857f9524d
woodsjc <woodsjc0@gmail.com>
2016-10-25 16:15:53 -0500

    updates to swap & x/words, but not working

--- crosswords.hs
+++ crosswords.hs
@@ -1,6 +1,5 @@
 import Data.Function   (on)
-import Data.List       (sort, sortBy)
-import Data.List.Split (splitOn)
+import Data.List       (sort, sortBy, isPrefixOf)
 import Data.Maybe
 
 main :: IO()
@@ -14,11 +13,14 @@ parse_crosswords str = reverse. sortBy (compare `on` length) $ splitOn ";" str
 
 
 --choosing the right crossword needs to be at a high level so it will remove from the potential list
---this should send back list of lengths & position [len, row, col]
+--this should send back list of lengths & position [len, start pos, end pos]
 parse_str :: Int -> String -> [Int]
 parse_str n [] = []
 parse_str n (x:xs)  
-           | x == '-'  = (\z -> [z,n,n+z] ++ parse_str (n+z) (pop z (x:xs))) $ len_section (x:xs)
+           | x == '-'  = (\z -> if z > 1
+                            then [z,n,n+z] ++ parse_str (n+z) (pop z (x:xs))
+                            else parse_str (n+z) (pop z (x:xs))
+                        ) $ len_section (x:xs)
            | otherwise = parse_str (n+1) xs 
 
 
@@ -35,37 +37,58 @@ len_section (s:ss)
            | otherwise = 0
 
 
-parse_across args = reverse $ sort $ map (parse_str 0) $ init args
-parse_down   args = reverse $ sort $ map (parse_str 0) $ 
-               map (\x -> map (!! x) $ init args) [0..((length $ init args)-1)]
-parse args   = (\x -> [(parse_across x)
-                      ,(parse_down x)
-                      ]
-                ) args
+parse_across :: [String] -> [[Int]]
+parse_across args = map (parse_str 0) $ init args
+
+
+parse_down :: [String] -> [[Int]]
+parse_down args = map (parse_str 0) $
+                      map (\x -> map (!! x) $ init args) 
+                          [0..((length $ init args)-1)]
 
 
 --not sure what type will end up being
 --likely need to rewrite parsing to include positions
 solve :: [String] -> [String]
-solve args = solve' (parse args) (parse_crosswords last args) (init args)
+solve args = solve' ((parse_across args) ++ [[0]])
+                    ((parse_down args) ++ [[0]])
+                    (parse_crosswords $ last args)
+                    (init args)
+
 
 --start up a recursive instance for each instance of across or down equal in length to crossword answer
-solve' [across,down] [] ans = ans
-solve' [across,down] cw ans 
-     | length (head across) == length (head cw) = solve' 
-                                                    [(tail across),down] (tail cw) 
-                                                    (fill_cw (head cw) (head across) ans)
-     | length (head down  ) == length (head cw) = solve' 
-                                                    [across,(tail down)] (tail cw) 
-                                                    (fill_cw (head cw) (head down) ans)
-     
+--if fill returns maybe need to backtrack or swap order of crosswords 
+--maybe always try to fit last of same length, but without the infinite loop
+--not sure how to preserve list when there isn't a hit in fill may need another parameter 
+--or just rerun parse on the updated ans
+--need to get the x&y position of the puzzle into fill (not currently happening)
+solve' :: [[Int]] -> [[Int]] -> [String] -> [String] -> [String]
+solve' across down [] ans = ans
+solve' []     []   cw ans = ans
+solve' across down cw ans 
+     | (\[z,_,_] -> z) (head across) == length (head cw) = (\x -> case x of 
+                                            Just x  -> solve' (tail across) down (tail cw) x
+                                            Nothing -> solve' (tail across) down cw        ans
+                                            ) $ fill_cw (head cw) (z, \(a1,a2,a3) -> (head across),(last across)) ans
+     | (\[z,_,_] -> z) (head down  ) == length (head cw) = (\x -> case x of
+                                            Just x  -> solve' across (tail down) (tail cw) x
+                                            Nothing -> solve' (tail across) down cw        ans
+                                            ) $ fill_cw (head cw) (last down) ans
+     | otherwise                                         = ans
+
+
+solve'' []     []   cw ans a_pos d_pos = ans
+solve'' across down [] ans a_pos d_pos = ans
+solve'' across down cw ans a_pos d_pos = 
+
+
 
 --fill should use info from match length
 --z > 0 is across and z <= 0 is down
 --need to recursively fill based on highest blanks in puzzle
 --not 100% sure how to deal with multiple matches
-fill_cw :: String -> (Int,Int,Int) -> [String] -> Maybe [String]
-fill_cw cw (x,y,z) puzzle 
+fill_cw :: String -> [Int] -> [String] -> Maybe [String]
+fill_cw cw [x,y,z] puzzle 
       | z >  0 = (\p -> if can_fill p y cw
                           then Just $ insert_list (fill p y cw) x puzzle
                           else Nothing
@@ -74,14 +97,13 @@ fill_cw cw (x,y,z) puzzle
                           then Just $ insert_list (fill p y cw) x (puzzle) --this needs to be sideways for down
                           else Nothing
                    ) $ map (!! x) puzzle
-      where
-        
+
 
 insert_list :: String -> Int -> [String] -> [String]
 insert_list str n puzzle = (\(ys,zs) -> 
                             if length zs > 0
-                                then ys ++ str ++ tail zs 
-                                else init ys ++ str) $ splitAt n puzzle 
+                                then ys ++ [str] ++ tail zs
+                                else init ys ++ [str]) $ splitAt n puzzle 
 
 
 can_fill :: String -> Int -> String -> Bool
@@ -92,6 +114,18 @@ can_fill str n cw = if str !! n == '-' || str !! n == head cw
                       else False
 
 
+--doesnt work with reverse need to use split & at
 fill :: String -> Int -> String -> String
-fill str n cw = take n str ++ cw ++ take (n-length cw) (reverse str)
+fill str n cw = (\(ys,zs) -> ys ++ cw ++ pop (length cw) zs) $ splitAt n str
 
+
+
+--http://stackoverflow.com/questions/5874600/cutting-a-string-into-a-list-in-haskell/5874939#5874939
+splitOn :: Eq a => [a] -> [a] -> [[a]]
+splitOn []    _  = error "splitOn: empty delimiter"
+splitOn delim xs = loop xs
+    where loop [] = [[]]
+          loop xs | delim `isPrefixOf` xs = [] : splitOn delim (drop len xs)
+          loop (x:xs) = let (y:ys) = splitOn delim xs
+                         in (x:y) : ys
+          len = length delim
--- swap-nodes.hs
+++ swap-nodes.hs
@@ -23,8 +23,28 @@ run_swaps [] _    = [[]]
 run_swaps xs tree = (\x -> [traverseBF x] ++ run_swaps (tail xs) x) $ swap_DF tree (head xs) 1
 
 
+--detect and padd empty places so build tree will work
+--take 15 [2^j-1 | j <- [1..]]
+convert_args :: [Int] -> [Int]
+convert_args nodes = if length nodes `elem` [1,3,7,15,31,63,127,255,511,1023,2047,4095,8191,16383]
+	then nodes
+	else pad nodes 0
+	where
+		pad nodes cur 
+		   | cur > node_max (length nodes) = nodes
+		   | nodes !! cur == -1            = pad (take (cur*2) nodes 
+		   	                                      ++ [-1,-1] 
+		   	                                      ++ drop (cur*2) nodes)
+		   	                                     (cur+1)
+		   | otherwise                     = pad nodes (cur+1)
+		node_max n_len = if n_len `elem` [1,3,7,15,31,63,127,255,511,1023,2047,4095,8191,16383]
+			then 0
+			else 2^(floor $ logBase 2 n_len)-1
+
+
 --nodes come in level order as list
 --[1,2,3,4] becomes r rl rr rll (and would have a lot of -1) [1,2,3,4,-1,-1,-1]
+--tree not actually symmetric, so need a new way to build
 buildTree []    = Empty
 buildTree nodes = if head nodes == -1 
 	then Empty 
@@ -34,7 +54,27 @@ buildTree nodes = if head nodes == -1
    where
    	left  = isLeft (length nodes - 1) 0
    	right = map not left 
-                  
+
+
+{-
+buildTree [] = Empty
+buildTree nodes = bt (tail nodes) [Node Empty (head nodes) Empty]
+	where
+		bt []            _     = Empty
+		bt _             []    = Empty
+		bt nodes@(x:y:z) queue = Node (Node Empty (left nodes) Empty)
+									  (head nodes) 
+									  (Node Empty (right nodes) Empty)
+		bt nodes queue         = Node (bt nodes queue) 
+									  (head nodes) 
+									  (bt nodes queue)
+	
+	  	clear_level _     []    = Empty
+		clear_level nodes level = Node ()
+		left = head $ tail
+		right = head $ tail $ tail
+-}
+
 
 separateLR :: [Bool] -> [a] -> [a]
 separateLR cond []    = []
@@ -45,10 +85,10 @@ separateLR cond nodes = (if (head cond) then [head nodes] else [])
 
 isLeft :: Int -> Int -> [Bool]
 isLeft len power
-  | len < (2^power) = take len $ replicate (2^power) True ++ replicate (2^power) False
-  | otherwise       = replicate (2^power) True 
-  					  ++ replicate (2^power) False 
-  					  ++ isLeft (len - 2^(power+1)) (power + 1)
+  | len <= (2^power) = take len $ replicate (2^power) True ++ replicate (2^power) False
+  | otherwise        = replicate (2^power) True 
+  					   ++ replicate (2^power) False 
+  					   ++ isLeft (len - 2^(power+1)) (power + 1)
 
 
 swap (Node left value right) = Node right value left
