a732ae17ecbbdf88a9845b15b2739cef93e6dfd5
woodsjc <woodsjc0@gmail.com>
2016-10-27 14:10:16 -0500

    passes some tests

--- swap-nodes.hs
+++ swap-nodes.hs
@@ -9,37 +9,48 @@ data Tree = Empty | Node Tree Int Tree deriving (Show, Eq)
 --middle 2*N elements of tree
 --after tree is the number of swaps
 --following the number of swaps is the depth at which to swap
+--understood 1 at start of tree (not mentioned anywhere)
 main :: IO()
 main = do
-	nodes <- fmap read getLine
-	tmp <- getContents
-	let args = map read $ concatMap words $ lines tmp 
-	mapM_ putStrLn $ map (unwords. map show)
-		  $ run_swaps (tail $ drop (nodes*2) args) $ buildTree $ take (nodes*2) args
+	_ <- getLine
+	tmp <- fmap ((map words). lines) getContents
+  	let 
+  		nodes = map read $ concat $ head $ split_args ([["1"]] ++ tmp)
+		swaps = tail $ map read $ concat $ head $ tail $ split_args tmp
+	mapM_ putStrLn $ map (unwords. map show) $ run_swaps swaps $ buildTree nodes
 	
 
+split_args :: [[String]] -> [[[String]]]
+split_args args = (\x -> [take (length args - length x) args] ++ [x] ) $ reverse $ s args
+	where
+		s [] = []
+		s y
+		 | length (last y) < 2 = (last y) : s (init y)
+		 | otherwise           = []
+
+
 run_swaps :: [Int] -> Tree -> [[Int]]
-run_swaps [] _    = [[]]
-run_swaps xs tree = (\x -> [traverseBF x] ++ run_swaps (tail xs) x) $ swap_DF tree (head xs) 1
+run_swaps []     _    = [[]]
+run_swaps [s]    tree = [inorder $ swap_DF tree s 1]
+run_swaps (s:ss) tree = (\x -> [inorder x] ++ run_swaps ss x)
+					$ swap_DF tree s 1
 
 
 --detect and padd empty places so build tree will work
 --take 15 [2^j-1 | j <- [1..]]
 convert_args :: [Int] -> [Int]
-convert_args nodes = if length nodes `elem` [1,3,7,15,31,63,127,255,511,1023,2047,4095,8191,16383]
-	then nodes
-	else pad nodes 0
+convert_args nodes = pad nodes 0
 	where
 		pad nodes cur 
 		   | cur > node_max (length nodes) = nodes
-		   | nodes !! cur == -1            = pad (take (cur*2) nodes 
+		   | nodes !! cur == -1            = pad (take ((cur+1)*2-1) nodes 
 		   	                                      ++ [-1,-1] 
-		   	                                      ++ drop (cur*2) nodes)
+		   	                                      ++ drop ((cur+1)*2-1) nodes)
 		   	                                     (cur+1)
 		   | otherwise                     = pad nodes (cur+1)
-		node_max n_len = if n_len `elem` [1,3,7,15,31,63,127,255,511,1023,2047,4095,8191,16383]
-			then 0
-			else 2^(floor $ logBase 2 n_len)-1
+		node_max n_len = if n_len > 2^14
+			then 0 
+			else fromIntegral (2^(floor $ logBase 2 (fromIntegral n_len))-1)
 
 
 --nodes come in level order as list
@@ -54,26 +65,6 @@ buildTree nodes = if head nodes == -1
    where
    	left  = isLeft (length nodes - 1) 0
    	right = map not left 
-
-
-{-
-buildTree [] = Empty
-buildTree nodes = bt (tail nodes) [Node Empty (head nodes) Empty]
-	where
-		bt []            _     = Empty
-		bt _             []    = Empty
-		bt nodes@(x:y:z) queue = Node (Node Empty (left nodes) Empty)
-									  (head nodes) 
-									  (Node Empty (right nodes) Empty)
-		bt nodes queue         = Node (bt nodes queue) 
-									  (head nodes) 
-									  (bt nodes queue)
-	
-	  	clear_level _     []    = Empty
-		clear_level nodes level = Node ()
-		left = head $ tail
-		right = head $ tail $ tail
--}
 
 
 separateLR :: [Bool] -> [a] -> [a]
@@ -113,15 +104,20 @@ traverseBF tree = tbf [tree]
 		tbf level = map get_value level ++ (tbf $ concat $ map get_children level)
 
 
-
+--swaps also occur at multiples of swap depth
 swap_DF :: Tree -> Int -> Int -> Tree 
 swap_DF cur depth c_depth
-	  | cur == Empty     = Empty
-      | c_depth == depth = swap cur
-      | c_depth <  depth = Node (swap_DF (get_left cur) depth (c_depth + 1))
-                                (get_value cur)
-                                (swap_DF (get_right cur) depth (c_depth + 1))
-      | otherwise	     = Empty
+	  | cur == Empty             = Empty
+      | c_depth `mod` depth == 0 = Node (swap_DF (get_right cur) depth (c_depth + 1))
+      									(get_value cur)
+      									(swap_DF (get_left cur) depth (c_depth + 1))
+      | otherwise                = Node (swap_DF (get_left cur) depth (c_depth + 1))
+                                        (get_value cur)
+                                        (swap_DF (get_right cur) depth (c_depth + 1))
+--      | c_depth <  depth         = Node (swap_DF (get_left cur) depth (c_depth + 1))
+--                                (get_value cur)
+--                                (swap_DF (get_right cur) depth (c_depth + 1))
+--      | otherwise	             = Empty
 
 
 inorder :: Tree -> [Int]
