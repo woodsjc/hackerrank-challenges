a340d109ce8968ebc73c58c53cdecf50c6e3a179
woodsjc <woodsjc0@gmail.com>
2016-11-18 09:32:22 -0600

    fix folder move

--- ai_section/stock_prediction.hs
+++ ai_section/stock_prediction.hs
@@ -28,23 +28,36 @@ get_current_price (Index name owned prices) = last prices
     
 index_rating :: Index -> Double
 index_rating (Index name owned prices) = last prices / (average $ init prices)
+    where range = take 3 $ drop 1 $ reverse prices
 
+--cap at 1/3 current money per stock
 actions :: Double -> [(Double, Index)] -> [String]
 actions _ [] = []
 actions m (i@(r,index):is) = (\buy_price -> 
     [buy_or_sell buy_price i] ++ actions (m-buy_price) is
-    ) $ max_buy m (get_current_price index)
+    ) $ max_buy m (get_current_price index) * 0.5
 
 --need a way to figure out how many to buy
 --take good deals & map onto avaliable (equally)
 buy_or_sell :: Double -> (Double, Index) -> String
 buy_or_sell money (rating, index@(Index name owned prices)) 
-   | rating >= 1.15 && owned > 0 = name ++ " SELL " ++ (show owned)
-   | rating <= 0.5 && money >= last prices = name ++ " BUY " 
-                                           ++ (show (floor $ money / (last prices)))
+   | rating >= 1.15 && owned > 0 = sell
+   | rating <= 0.5 && money >= last prices = buy
    | otherwise = ""
+   where buy = name ++ " BUY " ++ (show (floor $ money / (last prices)))
+         sell = name ++ " SELL " ++ (show owned)
 
 max_buy :: Double -> Double -> Double
 max_buy money price = if money > price 
-    then money + max_buy (money-money) price 
+    then price + max_buy (money-price) price 
     else 0.0
+
+get_prior_rating :: Index -> Double
+get_prior_rating (Index name owned prices) = (last prices) / (last $ tail prices)
+
+downward_indicator :: Index -> Bool
+downward_indicator (Index name owned prices) = 
+    if last prices > (last $ tail prices) 
+       && (last $ tail prices) > (last $ tail $ tail prices)
+         then True
+         else False
